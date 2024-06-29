b3a2bcc677320dbf83e645832e1e9f87ca521a89
Ivan Montenegro <ivanmontenegror@icloud.com>
2024-06-29 17:25:46 -0400

    Finished dynamically displaying default information.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-fetch('data.json')
+fetch('data/data.json')
 .then(response => response.json())
 .then(data => {
     createMenu(data);
@@ -16,11 +16,25 @@ function createMenu(data){
 
 function planetDefault(data, planet){
     const imageSection = document.getElementById('planet-image');
+    const nameSection = document.getElementById('planet-name');
+    const descSection = document.getElementById('planet-description');
+    const sourceSection = document.getElementById('planet-source');
     const specificPlanet =  data.find(item => item.name === planet);
     if (specificPlanet) { // Check if a planet was found
         const planetImage = document.createElement('img');
         planetImage.src = specificPlanet.images.planet;
         imageSection.appendChild(planetImage);
+        
+        const planetName = document.createElement('h2');
+        planetName.textContent = specificPlanet.name;
+        nameSection.appendChild(planetName);
+        
+        const planetDesc = document.createElement('p');
+        planetDesc.textContent = specificPlanet.overview.content;
+        descSection.appendChild(planetDesc);
+        
+        const planetSource = document.createElement('p');
+        planetSource.textContent = specificPlanet.overview.source;
+        sourceSection.appendChild(planetSource);
     }
-
 };
