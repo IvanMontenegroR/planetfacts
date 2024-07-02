522921a92179854ff592a59650dd9206b5936b44
Ivan Montenegro <ivanmontenegror@icloud.com>
2024-07-02 02:12:40 -0400

    Added dynamic change of content at menu item click.

--- js/script.js
+++ js/script.js
@@ -1,40 +1,63 @@
+let defaultPlanetFlag = 0;
+
 fetch('data/data.json')
 .then(response => response.json())
 .then(data => {
+    preloadImages(data);
+    
+    if(defaultPlanetFlag == 0){
+        planetDefault(data, "Earth");
+        defaultPlanetFlag = 1;
+    }
+
     createMenu(data);
-    planetDefault(data, "Earth");
 });
+
+function preloadImages(data) {
+    data.forEach(planet => {
+        const img = new Image();
+        img.src = planet.images.planet;
+    });
+}
 
 function createMenu(data){
     const menuList = document.getElementById('header-menu-elements');
     data.forEach(planet => {
+        const imageSection = document.getElementById('planet-image1');
+        const nameSectionText = document.getElementById('planet-name-text');
+        const descSectionText = document.getElementById('planet-desc-text');
+        const sourceSectionText = document.getElementById('planet-source-text');
         const newMenuItem = document.createElement('li');
+        
+        newMenuItem.classList.add('menu-item');
+        newMenuItem.setAttribute('data-planet-name', planet.name);
         newMenuItem.textContent = planet.name;
+
+        newMenuItem.addEventListener('click', function() {
+            imageSection.src = planet.images.planet;
+            nameSectionText.textContent = planet.name;
+            descSectionText.textContent = planet.overview.content;
+            sourceSectionText.textContent = `Source: ${planet.overview.source}`;
+            
+        });
+
         menuList.appendChild(newMenuItem);
-    })
-};
+    });
+}
 
 function planetDefault(data, planet){
-    const imageSection = document.getElementById('planet-image');
-    const nameSection = document.getElementById('planet-name');
-    const descSection = document.getElementById('planet-description');
-    const sourceSection = document.getElementById('planet-source');
+    const imageSection = document.getElementById('planet-image1');
+    const nameSectionText = document.getElementById('planet-name-text');
+    const descSectionText = document.getElementById('planet-desc-text');
+    const sourceSectionText = document.getElementById('planet-source-text');
     const specificPlanet =  data.find(item => item.name === planet);
     if (specificPlanet) { // Check if a planet was found
-        const planetImage = document.createElement('img');
-        planetImage.src = specificPlanet.images.planet;
-        imageSection.appendChild(planetImage);
+        imageSection.src = specificPlanet.images.planet;
         
-        const planetName = document.createElement('h2');
-        planetName.textContent = specificPlanet.name;
-        nameSection.appendChild(planetName);
+        nameSectionText.textContent = specificPlanet.name;
         
-        const planetDesc = document.createElement('p');
-        planetDesc.textContent = specificPlanet.overview.content;
-        descSection.appendChild(planetDesc);
+        descSectionText.textContent = specificPlanet.overview.content;
         
-        const planetSource = document.createElement('p');
-        planetSource.textContent = specificPlanet.overview.source;
-        sourceSection.appendChild(planetSource);
+        sourceSectionText.textContent = `Source: ${specificPlanet.overview.source}`;
     }
-};
+}
